908e3298d03e8bbb941cf70e4b6dfd31d2a5959a
Dhruvin Soni <dhruvin611@gmail.com>
2024-09-10 03:07:24 +0530

    2019_08_14_login_and_register

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/PizzaWale/static/end/end.controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+	'use strict';
+
+	app
+	.controller('endController', endController);
+
+	endController.$inject = ['$location', 'UserService','$localStorage'];
+	function endController($location, UserService, $localStorage) {
+		var vm=this;
+		$localStorage.email=vm.email;
+		$location.path('/') ;
+
+	}})();
---
+++ PizzaWale/WebContent/static/submit/login.controller.js
@@ -0,0 +1,47 @@
+﻿(function () {
+	'use strict';
+
+	app
+	.controller('SubmitController', SubmitController);
+
+	SubmitController.$inject = ['$location', 'UserService','$localStorage'];
+	function SubmitController($location, UserService,$localStorage) {
+		var vm = this;
+        console.log("inside SubmitController");
+		vm.login = login;
+	
+
+		(function initController() {
+		
+		})();
+		var user = {};
+		
+		function submit() {
+			  console.log("inside submit");
+
+			vm.dataLoading = true;
+			UserService.Submit()
+		    .then(function(response) {
+            	console.log(response) ;
+                if(response) {
+                	console.log("Submit success");
+                	
+                    $location.path('/login') ;
+                    alert("abdec");
+                }
+                else {
+                    vm.dataLoading = false ;
+                    alert("abc");
+                }
+            }) ;
+		    
+		
+		
+		
+	
+
+
+
+	}
+
+}})();
